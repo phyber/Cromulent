85fd13b2ffcc3f56c091029a228f00ea23df0c3a
phyber <phyber@wowace>
2018-08-12 14:26:13 +0100

    Tabs to spaces for Cromulent.lua

--- Cromulent.lua
+++ Cromulent.lua
@@ -1,5 +1,5 @@
 --[[
---	Credit to ckknight for originally writing Cartographer_ZoneInfo
+-- Credit to ckknight for originally writing Cartographer_ZoneInfo
 --]]
 Cromulent = LibStub("AceAddon-3.0"):NewAddon("Cromulent", "AceHook-3.0")
 local Cromulent, self = Cromulent, Cromulent
@@ -21,80 +21,79 @@ local FISHING_SKILL_FMT = "|cff%s%s|r |cff%s[%d]|r"
 -- Temporary until LibTourist knows what's up with fishing levels.
 local FISHING_ENABLED = false
 
---
 local WORLDMAP_CONTINENT = Enum.UIMapType.Continent
 
 -- World map IDs. Taken from WorldMapFrame:GetMapID()
 local WORLDMAP_AZEROTH_ID = 947
 
 function CromulentDataProviderMixin:OnAdded(mapCanvas)
-	MapCanvasDataProviderMixin.OnAdded(self, mapCanvas)
+    MapCanvasDataProviderMixin.OnAdded(self, mapCanvas)
 
-	if not self.Frame then
-		-- Create the frame and attach it to the WorldMap canvas container
-		self.Frame = CreateFrame(
-			"Frame",
-			nil,
-			self:GetMap():GetCanvasContainer()
-		)
+    if not self.Frame then
+        -- Create the frame and attach it to the WorldMap canvas container
+        self.Frame = CreateFrame(
+            "Frame",
+            nil,
+            self:GetMap():GetCanvasContainer()
+        )
 
-		-- Set the frame size
-		self.Frame:SetSize(400, 128)
+        -- Set the frame size
+        self.Frame:SetSize(400, 128)
 
-		-- Create a font string for the info text, using the WorldMap font
-		self.InfoText = self.Frame:CreateFontString(
-			nil,
-			"OVERLAY",
-			"WorldMapTextFont"
-		)
+        -- Create a font string for the info text, using the WorldMap font
+        self.InfoText = self.Frame:CreateFontString(
+            nil,
+            "OVERLAY",
+            "WorldMapTextFont"
+        )
 
-		local font, size = WorldMapTextFont:GetFont()
-		self.InfoText:SetFont(font, size, "OUTLINE")
+        local font, size = WorldMapTextFont:GetFont()
+        self.InfoText:SetFont(font, size, "OUTLINE")
 
-		-- Attach the infotext to the topleft of the frame and scale to 0.5.
-		self.InfoText:SetPoint("BOTTOMLEFT", self.Frame, "BOTTOMLEFT", 0, 0)
-		self.InfoText:SetScale(0.4)
-		self.InfoText:SetJustifyH("LEFT")
-	else
-		self.Frame:SetParent(self:GetMap():GetCanvasContainer())
-	end
+        -- Attach the infotext to the topleft of the frame and scale to 0.5.
+        self.InfoText:SetPoint("BOTTOMLEFT", self.Frame, "BOTTOMLEFT", 0, 0)
+        self.InfoText:SetScale(0.4)
+        self.InfoText:SetJustifyH("LEFT")
+    else
+        self.Frame:SetParent(self:GetMap():GetCanvasContainer())
+    end
 
-	-- Put the frame in the topleft of the world map
-	self.Frame:SetPoint(
-		"BOTTOMLEFT",
-		self:GetMap():GetCanvasContainer(),
-		10,
-		10
-	)
+    -- Put the frame in the topleft of the world map
+    self.Frame:SetPoint(
+        "BOTTOMLEFT",
+        self:GetMap():GetCanvasContainer(),
+        10,
+        10
+    )
 
-	self.Frame:SetFrameStrata("HIGH")
-	self.Frame.dataProvider = self
+    self.Frame:SetFrameStrata("HIGH")
+    self.Frame.dataProvider = self
 
-	-- Ensure everything is shown
-	self.Frame:Show()
-	self.InfoText:Show()
+    -- Ensure everything is shown
+    self.Frame:Show()
+    self.InfoText:Show()
 end
 
 function CromulentDataProviderMixin:RefreshAllData(fromOnShow)
-	local info = Cromulent:GetZoneInfo()
+    local info = Cromulent:GetZoneInfo()
 
-	if info then
-		self.InfoText:SetText(info)
-	else
-		self.InfoText:SetText("")
-	end
+    if info then
+        self.InfoText:SetText(info)
+    else
+        self.InfoText:SetText("")
+    end
 end
 
 function CromulentDataProviderMixin:RemoveAllData()
-	self.Frame:Hide()
+    self.Frame:Hide()
 end
 
 function Cromulent:OnEnable()
-	WorldMapFrame:AddDataProvider(CromulentDataProviderMixin)
+    WorldMapFrame:AddDataProvider(CromulentDataProviderMixin)
 end
 
 function Cromulent:OnDisable()
-	WorldMapFrame:RemoveDataProvider(CromulentDataProviderMixin)
+    WorldMapFrame:RemoveDataProvider(CromulentDataProviderMixin)
 end
 
 -- Text to display stored here.
@@ -104,152 +103,152 @@ local t = {}
 -- LibTourist is returning no fishing levels for now, so just return nil right
 -- away.
 local function getFishingInfo(mapId)
-	-- Fishing info in LibTourist always returns 0 for now, so disable this
-	-- by returning right away.
-	-- Hidden behind an if statement because otherwise Lua freaks out.
-	if not FISHING_ENABLED then
-		return nil
-	end
+    -- Fishing info in LibTourist always returns 0 for now, so disable this
+    -- by returning right away.
+    -- Hidden behind an if statement because otherwise Lua freaks out.
+    if not FISHING_ENABLED then
+        return nil
+    end
 
-	local fishingSkillText
-	local minFish = T:GetFishingLevel(zone)
+    local fishingSkillText
+    local minFish = T:GetFishingLevel(zone)
 
-	if minFish then
-		-- Get fishing index
-		-- prof1, prof2, archaeology, fishing, cooking, firstaid
-		local _, _, _, fishingIdx, _ = GetProfessions()
+    if minFish then
+        -- Get fishing index
+        -- prof1, prof2, archaeology, fishing, cooking, firstaid
+        local _, _, _, fishingIdx, _ = GetProfessions()
 
-		-- Find our current fishing rank
-		if fishingIdx then
-			local skillName, _, skillRank = GetProfessionInfo(fishingIdx)
-			local numColour = COLOUR_RED
-			if minFish < skillRank then
-				numColour = COLOUR_GREEN
-			end
+        -- Find our current fishing rank
+        if fishingIdx then
+            local skillName, _, skillRank = GetProfessionInfo(fishingIdx)
+            local numColour = COLOUR_RED
+            if minFish < skillRank then
+                numColour = COLOUR_GREEN
+            end
 
-			fishingSkillText = FISHING_SKILL_FMT:format(
-				COLOUR_YELLOW,
-				skillName,
-				numColour,
-				minFish
-			)
-		end
-	end
+            fishingSkillText = FISHING_SKILL_FMT:format(
+                COLOUR_YELLOW,
+                skillName,
+                numColour,
+                minFish
+            )
+        end
+    end
 
-	return fishingSkillText
+    return fishingSkillText
 end
 
 function Cromulent:GetZoneInfo()
-	local finalText
+    local finalText
 
-	-- Set the text to white and hide the zone info if we're on the Azeroth
-	-- continent map.
-	local mapID = WorldMapFrame:GetMapID()
-	local mapInfo = GetMapInfo(mapID)
-	local mapName = mapInfo.name
-	local zone = mapID
+    -- Set the text to white and hide the zone info if we're on the Azeroth
+    -- continent map.
+    local mapID = WorldMapFrame:GetMapID()
+    local mapInfo = GetMapInfo(mapID)
+    local mapName = mapInfo.name
+    local zone = mapID
 
-	if mapInfo.mapID == WORLDMAP_AZEROTH_ID then
-		if mapInfo.mapType == WORLDMAP_CONTINENT then
-			return
-		end
-	end
+    if mapInfo.mapID == WORLDMAP_AZEROTH_ID then
+        if mapInfo.mapType == WORLDMAP_CONTINENT then
+            return
+        end
+    end
 
-	-- Now we can do some real work if the zone is a real zone and/or has
-	-- instances
-	if zone and (T:IsZoneOrInstance(zone) or T:DoesZoneHaveInstances(zone)) then
-		local fishingSkillText = getFishingInfo(zone)
+    -- Now we can do some real work if the zone is a real zone and/or has
+    -- instances
+    if zone and (T:IsZoneOrInstance(zone) or T:DoesZoneHaveInstances(zone)) then
+        local fishingSkillText = getFishingInfo(zone)
 
-		-- List the instances in the zone if it has any.
-		if T:DoesZoneHaveInstances(zone) then
-			t[#t + 1] = ("|cffffff00%s:|r"):format(L["Instances"])
+        -- List the instances in the zone if it has any.
+        if T:DoesZoneHaveInstances(zone) then
+            t[#t + 1] = ("|cffffff00%s:|r"):format(L["Instances"])
 
-			-- Iterate over the instance list and insert them into t[]
-			for instance in T:IterateZoneInstances(zone) do
-				local complex = T:GetComplex(instance)
-				local low, high = T:GetLevel(instance)
-				local r1, g1, b1 = T:GetFactionColor(instance)
-				local r2, g2, b2 = T:GetLevelColor(instance)
-				local groupSize = T:GetInstanceGroupSize(instance)
-				local name = instance
+            -- Iterate over the instance list and insert them into t[]
+            for instance in T:IterateZoneInstances(zone) do
+                local complex = T:GetComplex(instance)
+                local low, high = T:GetLevel(instance)
+                local r1, g1, b1 = T:GetFactionColor(instance)
+                local r2, g2, b2 = T:GetLevelColor(instance)
+                local groupSize = T:GetInstanceGroupSize(instance)
+                local name = instance
 
-				if complex then
-					name = complex .. " - " .. instance
-				end
+                if complex then
+                    name = complex .. " - " .. instance
+                end
 
-				if low == high then
-					if groupSize > 0 then
-						t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d]|r " .. L["%d-man"]):format(
-							r1 * 255,
-							g1 * 255,
-							b1 * 255,
-							name,
-							r2 * 255,
-							g2 * 255,
-							b2 * 255,
-							high,
-							groupSize
-						)
-					else
-						t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d]|r"):format(
-							r1 * 255,
-							g1 * 255,
-							b1 * 255,
-							name,
-							r2 * 255,
-							g2 * 255,
-							b2 * 255,
-							high
-						)
-					end
-				else
-					if groupSize > 0 then
-						t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d-%d]|r " .. L["%d-man"]):format(
-							r1 * 255,
-							g1 * 255,
-							b1 * 255,
-							name,
-							r2 * 255,
-							g2 * 255,
-							b2 * 255,
-							low,
-							high,
-							groupSize
-						)
-					else
-						t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d-%d]|r"):format(
-							r1 * 255,
-							g1 * 255,
-							b1 * 255,
-							name,
-							r2 * 255,
-							g2 * 255,
-							b2 * 255,
-							low,
-							high
-						)
-					end
-				end
-			end
+                if low == high then
+                    if groupSize > 0 then
+                        t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d]|r " .. L["%d-man"]):format(
+                            r1 * 255,
+                            g1 * 255,
+                            b1 * 255,
+                            name,
+                            r2 * 255,
+                            g2 * 255,
+                            b2 * 255,
+                            high,
+                            groupSize
+                        )
+                    else
+                        t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d]|r"):format(
+                            r1 * 255,
+                            g1 * 255,
+                            b1 * 255,
+                            name,
+                            r2 * 255,
+                            g2 * 255,
+                            b2 * 255,
+                            high
+                        )
+                    end
+                else
+                    if groupSize > 0 then
+                        t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d-%d]|r " .. L["%d-man"]):format(
+                            r1 * 255,
+                            g1 * 255,
+                            b1 * 255,
+                            name,
+                            r2 * 255,
+                            g2 * 255,
+                            b2 * 255,
+                            low,
+                            high,
+                            groupSize
+                        )
+                    else
+                        t[#t + 1] = ("|cff%02x%02x%02x%s|r |cff%02x%02x%02x[%d-%d]|r"):format(
+                            r1 * 255,
+                            g1 * 255,
+                            b1 * 255,
+                            name,
+                            r2 * 255,
+                            g2 * 255,
+                            b2 * 255,
+                            low,
+                            high
+                        )
+                    end
+                end
+            end
 
-			-- Add the fishing info to t[] if it exists.
-			if fishingSkillText then
-				t[#t + 1] = fishingSkillText
-			end
+            -- Add the fishing info to t[] if it exists.
+            if fishingSkillText then
+                t[#t + 1] = fishingSkillText
+            end
 
-			-- OK, add all of the info from t[] into the zone info!
-			finalText = table_concat(t, "\n")
+            -- OK, add all of the info from t[] into the zone info!
+            finalText = table_concat(t, "\n")
 
-			-- Reset t[], ready for the next run.
-			table_wipe(t)
-		else
-			-- If the zone has no instances
-			-- Just set the fishing level text and be on our way.
-			if fishingSkillText then
-				finalText = fishingSkillText
-			end
-		end
-	end
+            -- Reset t[], ready for the next run.
+            table_wipe(t)
+        else
+            -- If the zone has no instances
+            -- Just set the fishing level text and be on our way.
+            if fishingSkillText then
+                finalText = fishingSkillText
+            end
+        end
+    end
 
-	return finalText
+    return finalText
 end
